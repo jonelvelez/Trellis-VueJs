8123ac1c259e3315a0144a3b80bb3021a185113a
Jonel <velezjonel22@gmail.com>
2021-01-12 11:22:03 +0800

    added shadow on header menu

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,10 @@ export default {
   box-sizing: border-box;
 }
 
+.navbar {
+  box-shadow: 0px 9px 20px 2px rgba(0,0,0,0.24);
+}
+
 .navbar-dark .navbar-toggler-icon {
   background-color: #666 !important;
 }
